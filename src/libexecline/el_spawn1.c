ea96addef47063dc6858c63f00e8e9731a31a990
Laurent Bercot <ska-skaware@skarnet.org>
2014-12-18 21:12:29 +0000

    Adapt el_spawn1 to new child_spawn1 in skalibs

--- src/libexecline/el_spawn1.c
+++ src/libexecline/el_spawn1.c
@@ -9,7 +9,7 @@ pid_t el_spawn1 (char const *prog, char const *const *argv, char const *const *e
   if (!argv[0])
   {
     static char const *const newargv[2] = { "/bin/true", 0 } ;
-    return child_spawn1(newargv[0], newargv, 0, fd, w) ;
+    return child_spawn1_pipe(newargv[0], newargv, 0, fd, w) ;
   }
-  else return child_spawn1(prog, argv, envp, fd, w) ;
+  else return child_spawn1_pipe(prog, argv, envp, fd, w) ;
 }
